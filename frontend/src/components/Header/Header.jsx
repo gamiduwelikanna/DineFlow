5bb1e3abb49cf21688e7cd378c3b495e83814ceb
Gamidu Welikanna <gamidukaveen@gmail.com>
2025-11-18 22:54:00 +0530

    frontend of the header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-contents'>
+        <h2>Order your favorite food here</h2>
+        <p>Explore a wide variety of cuisines and dishes from the comfort of your home.</p>
+        <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
-
+import Header from '../../components/Header/Header'  // Add this import
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Header />
+    </div>
   )
 }
 
