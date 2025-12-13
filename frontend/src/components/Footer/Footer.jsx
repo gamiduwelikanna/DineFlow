f0cb15e0430b71b4519e8d199bb443eca0a84c71
Gamidu Welikanna <gamiduwelikanna@Gamidus-MacBook-Air.local>
2025-12-13 23:08:52 +0530

    Frontend Dev on 13th Dec 25 -2

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import "./Footer.css"
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className='footer-content'>
+            <div className='footer-content-left'>
+                <img src={assets.logo} alt="" />
+                <p>Dine Flow simplifies the restaurant experience by connecting menus, orders, and reservations in one smooth platform.</p>
+                <div className='footer-social-icons'>
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.x_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className='footer-content-center'>
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div className='footer-content-right'>
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+ 94 112 121212</li>
+                    <li>contact.dineflow@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className='footer-copyright'>DineFlow © 2024. All rights reserved.</p>
+    </div> 
+  )
+}
+
+export default Footer
