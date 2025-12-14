128fdecc80f6001505c9e006f57ccfbe45e690ea
Gamidu Welikanna <gamiduwelikanna@Gamidus-MacBook-Air.local>
2025-12-14 12:47:59 +0530

    Frontend Dev on 14th Dec 25 -2

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 
   const[menu,setMenu] = useState("menu");
+  const[showMenu,setShowMenu] = useState(false);
+  
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className='logo'/>
-      <ul className='navbar-menu'>
-        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</li>
-        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</li>
-        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</li>
-        <li onClick={()=>setMenu("contact")} className={menu==="contact"?"active":""}>contact us</li>
+      <ul className={`navbar-menu ${showMenu ? 'active' : ''}`}>
+        <Link to='/' onClick={()=>{setMenu("home"); setShowMenu(false);}} className={menu==="home"?"active":""}>home</Link>
+        <a href='#explore-menu' onClick={()=>{setMenu("menu"); setShowMenu(false);}} className={menu==="menu"?"active":""}>menu</a>
+        <a href='#app-download' onClick={()=>{setMenu("mobile-app"); setShowMenu(false);}} className={menu==="mobile-app"?"active":""}>mobile-app</a>
+        <a href='#footer' onClick={()=>{setMenu("contact"); setShowMenu(false);}} className={menu==="contact"?"active":""}>contact us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt=""/>
@@ -21,6 +24,11 @@ const Navbar = () => {
           <div className="dot"></div>
         </div>
         <button>Sign In</button>
+        <div className={`hamburger ${showMenu ? 'active' : ''}`} onClick={()=>setShowMenu(!showMenu)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </div>
   )
