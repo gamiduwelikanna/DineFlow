6f9bc320d6a0211ade8a7a824e3dde97a36f7366
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-01-01 00:49:57 +0530

    Fixed the routing for the cart page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = ({setShowLogin}) => {
   
   return (
     <div className='navbar'>
-      <img src={assets.logo} alt="" className='logo'/>
+      <Link to='/'><img src={assets.logo} alt="" className='logo'/></Link>
       <ul className={`navbar-menu ${showMenu ? 'active' : ''}`}>
         <Link to='/' onClick={()=>{setMenu("home"); setShowMenu(false);}} className={menu==="home"?"active":""}>home</Link>
         <a href='#explore-menu' onClick={()=>{setMenu("menu"); setShowMenu(false);}} className={menu==="menu"?"active":""}>menu</a>
@@ -20,7 +20,7 @@ const Navbar = ({setShowLogin}) => {
       <div className="navbar-right">
         <img src={assets.search_icon} alt=""/>
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt=""/>
+          <Link to='/cart'><img src={assets.basket_icon} alt=""/></Link>
           <div className="dot"></div>
         </div>
         <button onClick={()=>setShowLogin(true)}>Sign In</button>
