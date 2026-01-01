2176aa501e12f0d20a1f1b65ea2d2a6d6818591d
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-01-01 19:45:52 +0530

    Making the UI of the Cart- upto the promocode is done without adding css

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -18,6 +18,22 @@ const Cart = () => {
         </div>
         <hr/>
         <br/>
+        {food_list.map((food) => {
+          if (cartItems[food._id] > 0) {
+            return (
+              <div key={food._id} className="cart-items-title cart-items-item">
+                <img src={food.image} alt={food.name} className="cart-item-image" />
+                <p className="cart-item-title">{food.name}</p>
+                <p className="cart-item-price">${food.price.toFixed(2)}</p>
+                <p className="cart-item-quantity">{cartItems[food._id]}</p>
+                <p className="cart-item-total">${(food.price * cartItems[food._id]).toFixed(2)}</p>
+                <button className="cart-item-remove" onClick={() => removeFromCart(food._id)}>Remove</button>
+              </div>
+            )
+          }
+          return null;
+        })}
+
       </div>
     </div>
   )
