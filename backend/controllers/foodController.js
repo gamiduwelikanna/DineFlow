ce3eb2041e96f41e4834dae24e83c4a6ca23ae9f
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-01-06 16:56:35 +0530

    feat : deleting a reorded response in the system is fixed

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -37,4 +37,16 @@ const listFood = async (req, res) => {
     }
 }
 
-export { addFoodItem, listFood };
+const removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`, () => {});
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({ success: true, message: "Food Item Removed Successfully" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error in Removing Food Item", error });
+    }
+}
+
+export { addFoodItem, listFood, removeFood };
--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addFoodItem , listFood} from '../controllers/foodController.js';
+import { addFoodItem , listFood, removeFood} from '../controllers/foodController.js';
 import multer from 'multer';
 
 const foodRouter = express.Router();
@@ -15,5 +15,6 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 foodRouter.post('/add',upload.single('image'), addFoodItem);
 foodRouter.get('/list', listFood);
+foodRouter.post('/remove', removeFood);
 
 export default foodRouter;
