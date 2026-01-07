8c4983ff0a6507611a60b548d0483e24c48f843e
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-01-08 00:26:00 +0530

    Fixed some css

--- backend/admin/src/Components/Navbar/Navbar.jsx
+++ backend/admin/src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,10 @@ import { assets } from "../../assets/assets";
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img className="logo" src={assets.logo} alt="Logo" />
+      <div className="navbar-left">
+        <img className="logo" src={assets.logo} alt="Logo" />
+        <h2 className="navbar-title">Admin Panel</h2>
+      </div>
       <img className="profile" src={assets.profile_image} alt="Profile" />
     </div>
   );
