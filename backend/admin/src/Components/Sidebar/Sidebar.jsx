ec1b2cd7d8ca0725b4a36b270273a49c9c82b5ec
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-01-08 12:23:52 +0530

    feat: making the structure

--- backend/admin/src/Components/Sidebar/Sidebar.jsx
+++ backend/admin/src/Components/Sidebar/Sidebar.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import './sidebar.css'
+import React from "react";
+import "./Sidebar.css";
+import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className="sidebar">
+      <div className="sidebar-options">
+        <div className="sidebar-option">
+          <img src={assets.add_icon} alt="Add" />
+          <p>Add Items</p>
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="Add" />
+          <p>List Items</p>
+        </div>
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="Add" />
+          <p>Orders</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
