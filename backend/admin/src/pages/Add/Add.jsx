58403b0ff98e374893cad84711755318fedc5ed5
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-01-09 07:44:08 +0530

    testing a commit

--- backend/admin/src/pages/Add/Add.jsx
+++ backend/admin/src/pages/Add/Add.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import "./Add.css"
+import React from 'react';
+import { assets } from '../../assets/assets';
 
 const Add = () => {
   return (
-    <div>Add</div>
-  )
-}
+    <div className="add">
+      <form className="flex-col">
+        {/* Image Upload */}
+        <div className="add-img-upload flex-col">
+          <p>Upload Image</p>
+          <label htmlFor="image">
+            <img src={assets.upload_area} alt="Upload Area" />
+          </label>
+          <input
+            type="file"
+            id="image"
+            hidden
+            required
+          />
+        </div>
 
-export default Add
+        {/* Product Name */}
+        <div className="add-product-name flex-col">
+          <p>Product Name</p>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter product name"
+            required
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Add;
