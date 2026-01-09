adc767b4d92733a2c1ef232883902d900e259ddc
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-01-10 00:27:39 +0530

    Add prduct page is being comlpleted

--- backend/admin/src/App.jsx
+++ backend/admin/src/App.jsx
@@ -5,13 +5,16 @@ import { Route, Routes } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
     <div>
+      <ToastContainer />
       <Navbar/>
       <hr/>
-      <div className='app-context'>
+      <div className='app-content'>
         <Sidebar/>
         <Routes>
           <Route path='/add' element={<Add/>}/>
