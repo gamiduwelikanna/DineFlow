9834c2760a1de59222c4587fa90a6ef10409d20b
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-01-10 13:32:13 +0530

    feat:login and register process

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,3 +1,14 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
 
-user Schema = 
+const userSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    cartData: {type: Object, default:{}},
+}, {
+    minimize: false
+});
+
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default userModel;
