a300bfa89cac029dd169be20925a53edd17da844
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-01-11 16:18:45 +0530

    fix:fixed the login data not saved after refreshing page issue

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -41,6 +41,12 @@ const StoreContextProvider = (props) => {
         return totalAmount;
     }
 
+    useEffect(() => {
+        if (localStorage.getItem("token")) {
+            setToken(localStorage.getItem("token"));
+        }
+    }, []);
+
     const contextValue = {
         food_list,
         cartItems,
