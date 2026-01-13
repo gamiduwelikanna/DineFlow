ea101112dad9caff0d1920f60ebf7054e8b094fb
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-01-13 21:33:15 +0530

    fix : minor fixes

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -2,11 +2,12 @@ import express from 'express';
 import { addToCart } from '../controllers/cartController';
 import { removeFromCart } from '../controllers/cartController.js';
 import { getCart } from '../controllers/cartController.js';
+import authMiddleware from '../middlware/auth.js';
 
 const cartRouter = express.Router();
 
-cartRouter.post('/add', addToCart);
-cartRouter.post('/remove', removeFromCart);
-cartRouter.get('/', getCart);
+cartRouter.post('/add',authMiddleware, addToCart);
+cartRouter.post('/remove',authMiddleware, removeFromCart);
+cartRouter.get('/',authMiddleware, getCart);
 
 export default cartRouter;
