d7aa8e4cbdfd581b2100966c9fc6eff43ccb561c
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-01-22 08:59:05 +0530

    feat: add to cart

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -5,7 +5,22 @@ import validator from "validator";
 
 
 //add items to cart
-const addToCart = async (req, res) => {}
+const addToCart = async (req, res) => {
+    try {
+        let UserData = await userModel.findOne({ _id: req.userId });
+        let cartData = await UserData.cartData;
+        if (!cartData[req.body.itemId]) {
+            cartData[req.body.itemId] = 1;
+        } else {
+            cartData[req.body.itemId] += 1;
+        }
+        UserData.cartData = cartData;
+        await userModel.findByIdAndUpdate(req.userId, { cartData});
+        res.status(200).json({ message: "Item added to cart"});
+    } catch (error) {
+        res.status(500).json({ message: "Error adding item to cart", error: error.message });
+    }
+}
 
 
 //remove items from cart
