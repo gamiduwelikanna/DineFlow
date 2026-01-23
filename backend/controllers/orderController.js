8066b390ef1fb748349fda0c81b7c0295a662497
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-01-23 17:44:56 +0530

    feat:adding the stripe secret key

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,9 @@
 import orderModel from '../models/orderModel.js';
 import userModel from '../models/userModel.js';
-import stripe from 'stripe';
+import Stripe from 'stripe';
 
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 //placing an order:fronrtend 
 const placeOrder = async (req, res) => { 
