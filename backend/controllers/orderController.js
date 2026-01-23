bf5984b3181933546924ebe635181c3d7f0fdaa9
Gamidu Welikanna <gamidukaveen@gmail.com>
2026-01-23 19:40:20 +0530

    feat:order related BR parts are done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,9 +5,42 @@ import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-//placing an order:fronrtend 
+//placing an order:frontend 
 const placeOrder = async (req, res) => { 
+    const frontend_url = "http://localhost:5173";
+    try{
+        const newOrder = new orderModel({
+            userId : req.body.userId,
+            items : req.body.items,
+            amount : req.body.amount,
+            address : req.body.address
+        });
+        await newOrder.save();
+        await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
 
+        const line_items = req.body.items.map((item) => ({
+            price_data: {
+                currency: 'usd',
+                product_data: {
+                    name: item.name,
+                },
+                unit_amount: item.price*100*80,
+            },
+            quantity: item.quantity,
+        }))
+
+        const session = await stripe.checkout.sessions.create({ 
+            line_items: line_items,
+            mode: 'payment',
+            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
+        });
+
+        res.status(200).json({ success: true, session_url: session.url });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: "Error placing order" });
+    }
 }
 
 export {placeOrder};
